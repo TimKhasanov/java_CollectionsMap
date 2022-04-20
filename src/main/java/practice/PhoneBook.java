acf32d6a4ea77f72cda65ab7291ab4a1bb2d8a32
Тимур Хасанов <tim.hasanov24@gmail.com>
2022-04-20 20:23:06 +0300

    updated PhoneBook class

--- src/main/java/practice/PhoneBook.java
+++ src/main/java/practice/PhoneBook.java
@@ -1,40 +1,56 @@
 package practice;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneBook {
+    private Map<String, String> map = new TreeMap<>();
+    private String regexPhone = "\\d{11}";
+    private String regexName = "^[a-zA-Zа-яА-Я]+$";
+    private Pattern patternPhone = Pattern.compile(regexPhone);
+    private Pattern patternName = Pattern.compile(regexName);
+
+    public boolean isPhone(String phone) {
+        Matcher matcherPhone = patternPhone.matcher(phone);
+        return matcherPhone.matches();
+    }
+
+    public boolean isName(String name) {
+        Matcher matcherName = patternName.matcher(name);
+        return matcherName.matches();
+    }
 
     public void addContact(String phone, String name) {
-        // проверьте корректность формата имени и телефона
-        // (рекомедуется написать отдельные методы для проверки является строка именем/телефоном)
-        // если такой номер уже есть в списке, то перезаписать имя абонента
+        if (isPhone(phone) && isName(name)) {
+            if (map.containsKey(phone)) {
+                map.replace(phone, map.get(phone), name);
+            } else {
+                map.put(phone, name);
+            }
+        }
     }
-
     public String getContactByPhone(String phone) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найдены - вернуть пустую строку
-        return "";
+        if (map.containsKey(phone)) {
+            return map.get(phone) + " - " + phone;
+        } else {
+            return "";
+        }
     }
-
     public Set<String> getContactByName(String name) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найден - вернуть пустой TreeSet
+        Set<String> contactName = new TreeSet<>();
         return new TreeSet<>();
     }
 
     public Set<String> getAllContacts() {
-        // формат одного контакта "Имя - Телефон"
-        // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
-    }
-
-    // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
-    // это поможет вам найти все ключи (key) по значению (value)
-    /*
-        for (Map.Entry<String, String> entry : map.entrySet()){
-            String key = entry.getKey(); // получения ключа
-            String value = entry.getValue(); // получения ключа
+        Set<String> keysAndValues = new TreeSet<>();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            keysAndValues.add(map.get(key) + " - " + key);
         }
-    */
+        return keysAndValues;
+    }
 }
